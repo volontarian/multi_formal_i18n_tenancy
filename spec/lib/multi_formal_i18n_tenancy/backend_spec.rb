42f423d394d636509a7ba79a57dff17916866996
mgawlista <mg@reqorder.com>
2012-07-13 20:35:08 +0200

    Adds new instance method available_locale to return the most compatible locale for formal address and / or multi tenancy inheritance setting to be used at locale switch through controller's before filter and / or optional config initializer. Adds support for locale files with more than 1 dot. Adds shared context :all_locale_file_constellations.

--- spec/lib/multi_formal_i18n_tenancy/backend_spec.rb
+++ spec/lib/multi_formal_i18n_tenancy/backend_spec.rb
@@ -7,16 +7,17 @@ require 'multi_formal_i18n_tenancy'
 
 describe MultiFormalI18nTenancy::Backend do
   before :all do
-    I18n.backend = MultiFormalI18nTenancy::Backend.new
-    I18n.load_path = [
-      File.expand_path('../../../fixtures/de.yml', __FILE__),
-      File.expand_path('../../../fixtures/de_formal.yml', __FILE__),
-      File.expand_path('../../../fixtures/tenants/your_tenant_name/your_tenant_name_de.yml', __FILE__),
-      File.expand_path('../../../fixtures/tenants/your_tenant_name/your_tenant_name_de_formal.yml', __FILE__)
-    ]
+    I18n.default_locale = :de
+    I18n.locale = I18n.default_locale
   end
   
-  describe '.translate' do
+  describe '#load_translations' do
+    include_context :all_locale_file_constellations
+    
+    before :all do
+      I18n.backend = MultiFormalI18nTenancy::Backend.new
+    end
+    
     context 'default' do
       context 'formal translation available' do
         it 'returns the formal translation' do
@@ -50,6 +51,140 @@ describe MultiFormalI18nTenancy::Backend do
           I18n.locale = :your_tenant_name_de_formal
           I18n.t('formal_unavailable').should == 'Du'
           I18n.t('formal_unavailable_again').should == 'Du auch wieder'
+        end
+      end
+      
+      context 'formal translation available for locale without base locale' do
+        it 'inits the locale without inheritation of unavailable base locale' do
+          I18n.locale = :another_tenant_name_fr_formal
+          I18n.t('formal_available').should == 'jamais vous'
+        end
+      end
+    end
+    
+    context 'locale files with more than 1 dot' do
+      it 'principally works' do
+        I18n.locale = :de
+        I18n.t('devise.example').should == 'Dein Beispiel' 
+        I18n.locale = :de_formal
+        I18n.t('devise.example').should == 'Ihr Beispiel'
+        I18n.locale = :your_tenant_name_de_formal
+        I18n.t('devise.informal_example').should == 'Dein anderes Beispiel' # devise.de.yml
+        I18n.t('devise.examples').should == 'Ihre Beispiele' # devise.de_formal.yml
+        I18n.t('devise.example').should == 'Ihr formelles Beispiel' # tenants/your_tenant_name/devise.your_tenant_name_de_formal.yml
+      end
+    end
+  end
+  
+  describe '.available_locale' do
+    context 'any locale' do
+      before :all do
+        I18n.load_path = [
+          File.expand_path('../../../fixtures/de.yml', __FILE__),
+          File.expand_path('../../../fixtures/en.yml', __FILE__)
+        ]
+      end
+      
+      # .available_locale any locale principally works
+      it 'principally works' do
+        I18n.backend = MultiFormalI18nTenancy::Backend.new
+        
+        [
+          [{}, :de], 
+          [{base_locale: :en}, :en], 
+          [{base_locale: :unavailable}, :de],
+          [{tenant: 'your_tenant_name'}, :de],
+          [{formal: true, tenant: 'your_tenant_name'}, :de],
+          [{formal: true, tenant: 'unavailable_tenant_name'}, :de]
+        ].each do |variant|
+          actual = I18n.backend.available_locale(variant.first)
+          actual.should(
+            be(variant.last), 
+            "input #{variant.first.inspect} should result in the output: #{variant.last.inspect} but got #{actual.inspect}"
+          )
+        end
+      end
+    end
+    
+    context 'any formal locale' do
+      before :all do
+        I18n.load_path = [
+          File.expand_path('../../../fixtures/de.yml', __FILE__),
+          File.expand_path('../../../fixtures/de_formal.yml', __FILE__)
+        ]
+      end
+      
+      it 'principally works' do
+        I18n.backend = MultiFormalI18nTenancy::Backend.new
+        
+        [
+          [{formal: true}, :de_formal], 
+          [{formal: true, tenant: 'your_tenant_name'}, :de_formal]
+        ].each do |variant|
+          actual = I18n.backend.available_locale(variant.first)
+          actual.should(
+            be(variant.last), 
+            "input #{variant.first.inspect} should result in the output: #{variant.last.inspect} but got #{actual.inspect}"
+          )
+        end
+      end
+    end
+    
+    context 'any formal locale plus tenant specific locale' do
+      before :all do
+        I18n.load_path = [
+          File.expand_path('../../../fixtures/de.yml', __FILE__),
+          File.expand_path('../../../fixtures/de_formal.yml', __FILE__),
+          File.expand_path('../../../fixtures/tenants/your_tenant_name/your_tenant_name_de.yml', __FILE__),
+          File.expand_path('../../../fixtures/tenants/your_tenant_name/your_tenant_name_en.yml', __FILE__)
+        ]
+      end
+      
+      it 'principally works' do
+        I18n.backend = MultiFormalI18nTenancy::Backend.new
+        
+        [
+          [{formal: true}, :de_formal], 
+          # should be :your_tenant_name_de_formal even though there is no :your_tenant_name_de_formal file 
+          # but the base file :de_formal to inherit from
+          [{formal: true, tenant: 'your_tenant_name'}, :your_tenant_name_de_formal],
+          [{base_locale: :en, formal: true, tenant: 'your_tenant_name'}, :your_tenant_name_en]
+        ].each do |variant|
+          actual = I18n.backend.available_locale(variant.first)
+          actual.should(
+            be(variant.last), 
+            "input #{variant.first.inspect} should result in the output: #{variant.last.inspect} but got #{actual.inspect}"
+          )
+        end
+      end
+    end
+    
+    context 'any formal tenant-specific locale' do
+      include_context :all_locale_file_constellations
+      
+      it 'principally works' do
+        I18n.backend = MultiFormalI18nTenancy::Backend.new
+      end
+    end 
+    
+    context 'tenant has special locale without an equivalent base locale' do
+      before :all do
+        I18n.load_path = [
+          File.expand_path('../../../fixtures/tenants/your_tenant_name/your_tenant_name_de_formal.yml', __FILE__)
+        ]
+      end
+      
+      it 'principally works' do
+        I18n.backend = MultiFormalI18nTenancy::Backend.new
+        
+        [
+          [{base_locale: :de, formal: true, tenant: 'your_tenant_name'}, :your_tenant_name_de_formal]
+        ].each do |variant|
+          actual = I18n.backend.available_locale(variant.first)
+          actual.should(
+            be(variant.last), 
+            "input #{variant.first.inspect} should result in the output: #{variant.last.inspect} but got #{actual.inspect}"
+          )
         end
       end
     end
